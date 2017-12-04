247e40c2a11a5de3d650b5419387b255a9260eaa
Prateek Lal <plal@nisum.com>
2017-12-04 14:59:12 +0530

    Implemented Google Sign In & Logout using Google Auth 2.0

--- app/views/loginView.js
+++ app/views/loginView.js
@@ -1,31 +1,65 @@
-define(['backbone','backbone.marionette','Templates', 'views/homeView'],function(backbone,marionette,templates,HomeView){
+define(['backbone','backbone.marionette','Templates', 'cryptojs'],function(backbone,marionette,templates, CryptoJS){
 	
 	var loginView = marionette.View.extend({
         el:'#main-content',
         template: _.template(templates.loginPageItemView),
         initialize: function () {
             this.render();
+            gapi.load('client:auth2', this.initClient);
+
         },
         render: function () {
             this.$el.html(this.template);
         },
         events:{
-            'click #login-btn':'onLoginAttempt'
+            'click #login-btn':'handleSignInClick',
         },
-        onLoginAttempt:function(event){
-            var self=this;
-            if (event) event.preventDefault();
-            console.log(this.$('#inputEmail').val());
-            console.log(this.$('#inputPassword').val());
-            backbone.history.navigate('home',true);
-           /* var navbarView = new NavbarView();
-            var headerRegion = navbarView.getRegion('headerRegion');
-            headerRegion.show(new NavbarView());*/
-            var homeView = new HomeView();
-            homeView.generateTable();
-            
+        initClient: function () {
+            // let context = this;
+            gapi.client.init({
+                apiKey: "AIzaSyD4swrOLLYbcI54A_PmkLfLbcIpO_vGk_Q",
+                discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+                clientId: "545604378019-4msm2qi82vbsqiga80clmqf3fkdv4ms2.apps.googleusercontent.com",
+                scope: "profile",
+                // hostedDomain: "nisum.com"
+            }).then(function () {
+                // Listen for sign-in state changes.
+                gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+                // Handle the initial sign-in state.
+                updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+                // Create a function to handle the state
+                function updateSigninStatus (isSignedIn) {
+                    let googleUser = gapi.auth2.getAuthInstance().currentUser.get();
+                    let basicProfile = googleUser.getBasicProfile();
+                    if(isSignedIn) {
+                        gapi.client.people.people.get({
+                            'resourceName': 'people/me',
+                            'requestMask.includeField': 'person.names'
+                        }).then(function (response) {
+                            if (googleUser.getHostedDomain() === "nisum.com") {
+                                localStorage.setItem("loggedIn", true);
+                                localStorage.setItem("userId", CryptoJS.MD5(basicProfile.getId()));
+                                localStorage.setItem("userName", basicProfile.getName());
+                                localStorage.setItem("userEmail", basicProfile.getEmail());
+                                localStorage.setItem("userImage", basicProfile.getImageUrl());
+                                backbone.history.navigate('home',true);
+                            } else {
+                                console.error("You are not authorised to access the portal. Please request access from the administrator or login with correct credentials");
+                            }
+
+                        }, function (reason) {
+                            console.log('Error: ' + reason.result.error.message);
+                        })
+                    }
+                }
+            });
+        },
+        handleSignInClick: function(event) {
+            gapi.auth2.getAuthInstance().signIn();
         }
     });
 
     return loginView;
-})
+});
